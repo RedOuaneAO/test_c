10ad865999dc4d871726905de20b23bc4ef395bb
RedOuaneAO <redouaneao97@gmail.com>
2022-09-07 16:55:30 +0100

    chal4

---
+++ challenge4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(){
+	float Mile , Km ;
+	printf("write distance Mile :");
+	scanf("%f", &Mile);
+	Km= Mile*1.609 ; 
+	printf("distance Km %f", Km);
+	
+}
